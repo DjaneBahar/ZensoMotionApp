af2804908511ca8e64b0fd0d20471da52cb6a015
sunetb <sunetb@gmail.com>
2020-10-15 23:18:06 +0200

    Knap-variabel oprettet

--- app/src/main/java/com/example/application/navigationapp/ui/LeaderboardFragment.java
+++ app/src/main/java/com/example/application/navigationapp/ui/LeaderboardFragment.java
@@ -7,13 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.application.navigationapp.R;
 
 
 public class LeaderboardFragment extends Fragment {
 
-
+    Button suneKnap;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
